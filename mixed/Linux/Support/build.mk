992280468c25cedfe257a904b19dcb7ff930d977
Timothy E Baldwin <T.E.Baldwin99@members.leeds.ac.uk>
2015-03-18 00:58:25 +0000

    Linux: Add Makefile to build RISC OS on Linux

---
+++ mixed/Linux/Support/build.mk
@@ -0,0 +1,95 @@
+#!/usr/bin/make -f
+
+HARDDISC4=${HOME}/Downloads/HardDisc4.5.22.zip
+QEMU_SRC=${HOME}/Downloads/qemu-2.8.0.tar.bz2
+RPCEMU=${HOME}/Downloads/rpcemu-0.8.15.tar.gz
+IOMD=${HOME}/Downloads/IOMD-Soft.5.22.zip
+
+LINUX_ROM=mixed/Linux/Support/RISC_OS
+ACORN_CPP=../DDE/AcornC.C++
+
+QEMU=$(shell uname -m | grep -q arm || echo qemu/arm-linux-user/qemu-arm)
+
+TARGET=Linux
+METHOD=Linux
+PHASES=export_hdrs export_libs resources rom install_rom join
+
+RPCEMU_BIN=rpcemu/rpcemu
+BOOT_IOMD_ROM=boot_iomd_rom
+
+fd_BOOT_IOMD_ROM=7
+fd_BUILD_DIR=8
+fd_ACORN_CPP=9
+
+SHELL=/bin/bash
+.SHELLFLAGS=-e -c
+
+.ONESHELL:
+.DELETE_ON_ERROR:
+.PHONY: all clean
+
+# *Set Alias$$Obey %%Obey -v %%*0\n
+
+ifeq (${METHOD}, rpcemu)
+all: rpcemu/rpcemu boot_iomd_rom stamp-prepare
+	rm "Images/${TARGET}_rom" || true
+	test ! -f done* || rm done*
+	echo '*Obey mixed.Linux.Support.Build rpcemu ${TARGET} ${PHASES}' > '!Boot,fea'
+	rpcemu/rpcemu ${fd_BOOT_IOMD_ROM}< boot_iomd_rom \
+	  ${fd_BUILD_DIR}<. ${fd_ACORN_CPP}<${ACORN_CPP} &
+	until test -f done*; do sleep 1; done
+	kill $$!; sleep 1
+	sh done*
+	rm done*
+	mv "Images/${TARGET}_rom",??? "Images/${TARGET}_rom" || true
+else
+all: ${QEMU} ${LINUX_ROM} comma2attr stamp-prepare
+	rm "Images/${TARGET}_rom" || true
+	find * -depth -exec ./comma2attr -- '{}' + ${fd_ACORN_CPP}<${ACORN_CPP} || true
+	RISC_OS_Alias_IXFSBoot='Exec IXFS:$$.dev.fd.4' ${QEMU} ${LINUX_ROM} --nofork \
+	${fd_BUILD_DIR}<. ${fd_ACORN_CPP}<${ACORN_CPP} 4< <(
+	echo '*BASIC
+	*FX 3 2
+	*Dir IXFS:$$.dev.fd.${fd_BUILD_DIR}
+	PRINT TIME$$
+	TIME$$="$(shell date --utc "+%a,%0e %b %Y.%T")"
+	QUIT
+	*Obey -c mixed.Linux.Support.Build Linux ${TARGET} ${PHASES}
+	' )
+endif
+	chmod +x "Images/${TARGET}_rom"
+	ln -sfn Images/Linux_rom RISC_OS || true
+
+ifeq (${TARGET}, IOMD32)
+check: rpcemu/rpcemu HardDisc4/stamp all
+	echo "Create HostFS:$$.done" > HardDisc4/\!Boot/Choices/Boot/Tasks/ZZZ,feb
+	test ! -f HardDisc4/done* || rm HardDisc4/done*
+	timeout -sKILL 20 rpcemu/rpcemu ${fd_BOOT_IOMD_ROM}<"Images/${TARGET}_rom" ${fd_BUILD_DIR}<HardDisc4 || true
+	sleep 1
+	rm HardDisc4/\!Boot/Choices/Boot/Tasks/ZZZ,feb
+	test -f HardDisc4/done*
+else
+check: all ${QEMU}
+	printf '*BASIC\n*ChangeDynamicArea -ramfssize 8M\nREPEAT:UNTIL TIME>10:SYS "IXSupport_LinuxSyscall",,,,,,,,1\n!0=0\n' | timeout -sKILL 10 ${QEMU} "Images/${TARGET}_rom"
+endif
+
+fast: PHASES=install_rom join
+fast: check
+
+stamp-prepare:
+	ln -sfn  mixed/RiscOS/Library
+	ln -sfn  mixed/RiscOS/Modules
+	ln -sfn castle/RiscOS/Env
+	ln -sfn castle/RiscOS/BuildSys
+	cp -a castle/RiscOS/Export .
+	mkdir -p Apps
+	(cd Apps && ln -sfn ../*/RiscOS/Apps/\!* .)
+	for i in amu c++ cc cfront cmhg link decaof libfile objasm ResGen; do \
+	  ln -sfn "/proc/self/fd/${fd_ACORN_CPP}/!SetPaths/Lib32/$$i,ff8" Library/Acorn/$$i,ff8; \
+	done
+	mkdir -p Export/APCS-32/Lib/CLib/o
+	ln -sfn '/proc/self/fd/${fd_ACORN_CPP}/Libraries/c++lib' Export/APCS-32/Lib/c++lib
+	ln -sfn '/proc/self/fd/${fd_ACORN_CPP}/Libraries/CLib/o/stubs,ffd' Export/APCS-32/Lib/CLib/o/stubs_bootstrap
+	touch stamp-prepare
+
+include mixed/Linux/Support/common.mk
---
+++ mixed/Linux/Support/common.mk
@@ -0,0 +1,65 @@
+comma2attr: mixed/Linux/Support/comma2attr.c
+	gcc -std=gnu99 -g `pwd`/mixed/Linux/Support/comma2attr.c -o comma2attr
+
+mixed/Linux/Support/RISC_OS: mixed/Linux/Support/RISC_OS.xz
+	xz --decompress --keep mixed/Linux/Support/RISC_OS.xz
+
+rpcemu/stamp: ${RPCEMU}
+	rm -rf rpcemu rpcemu-0.8.15 || true
+	echo "3f092e6000b5d50984e63768a74cdc9a40284d55c984e26df44c7d5875ced6e9 *${RPCEMU}" | sha256sum -c
+	tar zxf "${RPCEMU}"
+	mv rpcemu-0.8.15 rpcemu
+	touch rpcemu/stamp
+
+rpcemu/src/Makefile: rpcemu/stamp
+	(cd rpcemu/src && ./configure `uname -m | grep -E -q 'x86|i386' && echo --enable-dynarec` CFLAGS="-no-pie -fno-pie")
+
+rpcemu/rpcemu: rpcemu/src/Makefile
+	$(MAKE) -C rpcemu/src
+	rm rpcemu/poduleroms/SyncClock,ffa
+	touch rpcemu/rpcemu
+	echo H4sIAISQCFUCA2NgYDNTuO7MAAdlTAEM+Q4MDgwaNgxoQABdgOHVGoZ/DK8ZZoJlQxQ4uHQmMDGw\
+	wWSZGWgP+EUYGJb8FXD8z5jIIAgV+0CMRm5GBgYTBgYAXYSkcwABAAA= | base64 -d | gzip -d > rpcemu/cmos.ram
+	cp mixed/Linux/Support/rpcemu.cfg rpcemu/rpc.cfg
+	rm -rf rpcemu/hostfs || true
+	ln -sfn /proc/self/fd/${fd_BUILD_DIR} rpcemu/hostfs
+	ln -sfn /proc/self/fd/${fd_BOOT_IOMD_ROM} rpcemu/roms/ROM
+
+qemu/stamp: ${QEMU_SRC}
+	rm -rf qemu qemu-2.8.0
+	echo "dafd5d7f649907b6b617b822692f4c82e60cf29bc0fc58bc2036219b591e5e62 *${QEMU_SRC}" | sha256sum -c
+	tar jxf "${QEMU_SRC}"
+	mv qemu-2.8.0 qemu
+	(cd qemu && patch -p1 < ../mixed/Linux/Support/qemu_swi.diff)
+	touch qemu/stamp
+
+qemu/Makefile: qemu/stamp
+	(cd qemu && ./configure --enable-attr --target-list=arm-linux-user --disable-werror)
+	touch qemu/Makefile
+
+qemu/arm-linux-user/qemu-arm: qemu/Makefile
+	$(MAKE) -C qemu
+
+HardDisc4/stamp: ${HARDDISC4} boot_iomd_rom rpcemu/rpcemu
+	echo '2b5c2eadb4b4d5cff1ae5dfbce1159c15b41720ef89dcf735062d86074f34083 *${HARDDISC4}' | sha256sum -c
+	rm -rf HardDisc4 || true
+	unzip -F '${HARDDISC4}' 'HardDisc4/*'
+	timeout -sKILL 20 rpcemu/rpcemu ${fd_BOOT_IOMD_ROM}<boot_iomd_rom ${fd_BUILD_DIR}<HardDisc4 || true
+	touch HardDisc4/stamp
+
+boot_iomd_rom: ${IOMD}
+	echo '18d56d246fea44e856f11b1173b1f132950e8e8aa145a8cf83836e28061b8544 *${IOMD}' | sha256sum -c
+	unzip -p '${IOMD}' "soft/!Boot/Choices/Boot/PreDesk/!!SoftLoad/riscos" > boot_iomd_rom
+	echo '222f7e5443e48071e1b41a1c8098071c6afc9a39973dfe17374aa1a2580df6a3 *boot_iomd_rom' | sha256sum -c
+
+${HARDDISC4}:
+	sh mixed/Linux/Support/download.sh '${HARDDISC4}' "https://www.riscosopen.org/zipfiles/platform/common/HardDisc4.5.22.zip?1429452894" "2b5c2eadb4b4d5cff1ae5dfbce1159c15b41720ef89dcf735062d86074f34083"
+
+${IOMD}:
+	sh mixed/Linux/Support/download.sh '${IOMD}' "https://www.riscosopen.org/zipfiles/platform/riscpc/IOMD-Soft.5.22.zip?1429651024" "18d56d246fea44e856f11b1173b1f132950e8e8aa145a8cf83836e28061b8544"
+
+${QEMU_SRC}:
+	sh mixed/Linux/Support/download.sh '${QEMU_SRC}' "http://wiki.qemu-project.org/download/qemu-2.8.0.tar.bz2" "dafd5d7f649907b6b617b822692f4c82e60cf29bc0fc58bc2036219b591e5e62"
+
+${RPCEMU}:
+	sh mixed/Linux/Support/download.sh '${RPCEMU}' "http://www.marutan.net/rpcemu/cgi/download.php?sFName=0.8.15/rpcemu-0.8.15.tar.gz" "3f092e6000b5d50984e63768a74cdc9a40284d55c984e26df44c7d5875ced6e9"
