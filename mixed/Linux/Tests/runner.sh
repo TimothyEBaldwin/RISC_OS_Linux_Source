a5c683446590dc73bbbe596150c2a958ec36b223
Timothy E Baldwin <T.E.Baldwin99@members.leeds.ac.uk>
2017-03-15 22:27:53 +0000

    Linux Tests: BASIC self-test

--- mixed/Linux/Tests/runner.sh
+++ mixed/Linux/Tests/runner.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 set -e
-(
-timeout --foreground -sKILL 6 $* --nofork --noaborts << 'END' 2>&1 | tee /dev/fd/2 | grep -q GOOD
-*ChangeDynamicArea -ramfssize 6M
-*ChangeDynamicArea -ramfssize 4M
-*ChangeDynamicArea -ramfssize 8M
-*Echo GO<79>D
+
+timeout --foreground -sKILL 30 $* --nofork --noaborts << 'END'
+*BASIC
+1 *ChangeDynamicArea -ramfssize 6M
+2 *ChangeDynamicArea -ramfssize 4M
+3 *ChangeDynamicArea -ramfssize 8M
+4 TIME=0
+5 REPEAT:UNTIL TIME>10
+6 A%=0:H%=1:CALL &33000050
+RUN
+!0=0
 END
-echo
-) 2>&1
