c36b8e14a8b4bd3cefd1207026c6ed3f87619024
Timothy E Baldwin <T.E.Baldwin99@members.leeds.ac.uk>
2017-03-19 21:44:18 +0000

    Linux Tests: Test register preservation when running transient utilities.

--- mixed/Linux/Tests/runner.sh
+++ mixed/Linux/Tests/runner.sh
@@ -4,13 +4,16 @@ set -e
 
 timeout --foreground -sKILL 30 $* --nofork --noaborts << 'END'
 *BASIC
+0 ON ERROR PRINT REPORT$;" at line ";ERL:?0=0
 1 *ChangeDynamicArea -ramfssize 6M
 2 *ChangeDynamicArea -ramfssize 4M
 3 *ChangeDynamicArea -ramfssize 8M
 4 TIME=0
 5 REPEAT:UNTIL TIME>10
-6 SYS "IXSupport_LinuxSyscall",,,,,,,,1
 RUN
+TEXTLOAD "IXFS:$.proc.self.cwd.mixed.Linux.Tests.TTest"
+RUN
+SYS "IXSupport_LinuxSyscall",,,,,,,,1
 !0=0
 END
 
