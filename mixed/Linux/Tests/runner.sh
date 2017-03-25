f82e062aca25f7876410e35c2064147e917a0749
Timothy E Baldwin <T.E.Baldwin99@members.leeds.ac.uk>
2017-03-25 11:26:25 +0000

    Linux Tests: Make more uniform and portable

--- mixed/Linux/Tests/runner.sh
+++ mixed/Linux/Tests/runner.sh
@@ -3,20 +3,8 @@
 set -e
 
 find mixed/Linux/Tests -depth -exec ./comma2attr -- '{}' +
+export RISC_OS_Test_Dir='IXFS:$.proc.self.cwd.mixed.Linux.Tests'
 
-timeout --foreground -sKILL 30 $* --nofork --noaborts << 'END'
-*BASIC
-0 ON ERROR PRINT REPORT$;" at line ";ERL:?0=0
-1 *ChangeDynamicArea -ramfssize 6M
-2 *ChangeDynamicArea -ramfssize 4M
-3 *ChangeDynamicArea -ramfssize 8M
-4 TIME=0
-5 REPEAT:UNTIL TIME>10
-RUN
-TEXTLOAD "IXFS:$.proc.self.cwd.mixed.Linux.Tests.TTest"
-RUN
-SYS "IXSupport_LinuxSyscall",,,,,,,,1
-!0=0
-END
+RISC_OS_Alias_IXFSBoot='Obey -v <Test$Dir>.PreDesk_NoAbort' timeout --foreground -sKILL 30 $* --nofork --noaborts
 
 RISC_OS_Alias_IXFSBoot="Exec IXFS:$.proc.self.cwd.mixed.Linux.Tests.Exec" timeout --foreground -sKILL 60 $* --nofork 3>/dev/null 2>&1
