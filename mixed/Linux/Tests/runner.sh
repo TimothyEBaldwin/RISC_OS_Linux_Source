5e74fef59f151eadad6f9372fef2c8042c172ca4
Timothy E Baldwin <T.E.Baldwin99@members.leeds.ac.uk>
2017-12-21 18:24:26 +0000

    Linux Tests: Use Test$Dir in more places

--- mixed/Linux/Tests/runner.sh
+++ mixed/Linux/Tests/runner.sh
@@ -6,4 +6,4 @@ export RISC_OS_Test_Dir='IXFS:$.proc.self.cwd.mixed.Linux.Tests'
 
 RISC_OS_Alias_IXFSBoot='Obey -v <Test$Dir>.PreDesk_NoAbort' timeout --foreground -sKILL 30 $* --nofork --noaborts
 
-RISC_OS_Alias_IXFSBoot="Exec IXFS:$.proc.self.cwd.mixed.Linux.Tests.Exec" timeout --foreground -sKILL 60 $* --nofork 3>/dev/null 2>&1
+RISC_OS_Alias_IXFSBoot='Exec <Test$Dir>.Exec' timeout --foreground -sKILL 60 $* --nofork 3>/dev/null 2>&1
