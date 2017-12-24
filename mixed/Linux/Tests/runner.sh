31bd3e784eba4375f733e80e68f804b627cfc131
Timothy E Baldwin <T.E.Baldwin99@members.leeds.ac.uk>
2017-12-24 22:55:05 +0000

    Linux Tests: Support building on filing systems that don't support POSIX extended attributes.

--- mixed/Linux/Tests/runner.sh
+++ mixed/Linux/Tests/runner.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-find mixed/Linux/Tests -depth -exec ./comma2attr -- '{}' +
 export RISC_OS_Test_Dir='IXFS:$.proc.self.cwd.mixed.Linux.Tests'
 
 RISC_OS_Alias_IXFSBoot='Obey -v <Test$Dir>.PreDesk_NoAbort' timeout --foreground -sKILL 30 $* --nofork --noaborts
