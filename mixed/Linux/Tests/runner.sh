53a4fb9d6940e2b9ac4c732fc9bfa9196f51833d
Timothy E Baldwin <T.E.Baldwin99@members.leeds.ac.uk>
2018-01-01 15:23:51 +0000

    Linux Tests: Run tests in private namespace

--- mixed/Linux/Tests/runner.sh
+++ mixed/Linux/Tests/runner.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+if [ "$RISC_OS_Test_NetOK" == "YES" ]; then
+  # Configure network
+  ip link set lo up
+else
+  # Create namespace if possible
+  if unshare --user --net --mount --pid --fork --mount-proc --map-root-user true; then
+    export RISC_OS_Test_NetOK=YES
+    exec unshare --user --net --mount --pid --fork --mount-proc --map-root-user $0 $*
+  fi
+fi
+
 export RISC_OS_Test_Dir='IXFS:$.proc.self.cwd.mixed.Linux.Tests'
 
 RISC_OS_Alias_IXFSBoot='Obey -v <Test$Dir>.PreDesk_NoAbort' timeout --foreground -sKILL 30 $* --nofork --noaborts
