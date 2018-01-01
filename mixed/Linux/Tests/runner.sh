137c0e8e49bb802e7e7faa684ba4a1d9e6151c60
Timothy E Baldwin <T.E.Baldwin99@members.leeds.ac.uk>
2018-01-01 15:21:24 +0000

    Linux Tests: Try also with no --seccomp also.

--- mixed/Linux/Tests/runner.sh
+++ mixed/Linux/Tests/runner.sh
@@ -6,4 +6,6 @@ export RISC_OS_Test_Dir='IXFS:$.proc.self.cwd.mixed.Linux.Tests'
 
 RISC_OS_Alias_IXFSBoot='Obey -v <Test$Dir>.PreDesk_NoAbort' timeout --foreground -sKILL 30 $* --nofork --noaborts
 
+RISC_OS_Alias_IXFSBoot='Obey -v <Test$Dir>.PreDesk_NoAbort' timeout --foreground -sKILL 30 $* --noseccomp --nofork --noaborts
+
 RISC_OS_Alias_IXFSBoot='Obey -v <Test$Dir>.PreDesk_Aborts' timeout --foreground -sKILL 60 $* --nofork 3>/dev/null 2>&1
