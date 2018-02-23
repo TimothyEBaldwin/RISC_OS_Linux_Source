22de9e41e0a65333c94f118f3142bc1736eb2d67
Timothy E Baldwin <T.E.Baldwin99@members.leeds.ac.uk>
2018-02-23 18:37:09 +0000

    Linux Support + Tests: Sandbox build
    + Use Bubblewrap and SECCOMP to protect Linux environment from
      RISC OS and RISC OS programs (including those in the source).
    + Perform build in tree of symbolic links to prevent
      modification of source code.
    + Moved built Linux tools to directory "Built".
    + Symbolic link "Support" to "mixed/Linux/Support" for
      consistency with binary distribution.
    + Provide script and desktop file to run RISC OS.
    + Rearrange make files so that "common.mk" can standalone.
    + Linux tests require unprivileged user namespaces,
      skipped if not available.
    + Ship a copy of Keyboard.h to avoid dependency problems and the
      risk of RISC OS malware in the source tree using it to escape.
    + Make builds parallel by default.

--- mixed/Linux/Support/build.mk
+++ mixed/Linux/Support/build.mk
@@ -1,91 +1,154 @@
-#!/usr/bin/make -f
+#
+# Copyright (c) 2013-2018, Timothy Baldwin
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of RISC OS Open Ltd nor the names of its contributors
+#       may be used to endorse or promote products derived from this software
+#       without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
 
-HARDDISC4=${HOME}/Downloads/HardDisc4.5.22.zip
-QEMU_SRC=${HOME}/Downloads/qemu-2.11.0.tar.xz
-RPCEMU=${HOME}/Downloads/rpcemu-0.8.15.tar.gz
-IOMD=${HOME}/Downloads/IOMD-Soft.5.22.zip
 
-LINUX_ROM=mixed/Linux/Support/bin/!Boot/Linux/RISC_OS
+LINUX_ROM=Support/bin/!Boot/Linux/RISC_OS
 ACORN_CPP=../DDE/AcornC.C++
-
-#JOBS=$(shell getconf _NPROCESSORS_ONLN)
-export JOBS
-QEMU=$(shell if uname -m | grep -q -E 'arm|aarch64'; then which env; else echo qemu/arm-linux-user/qemu-arm; fi)
 
 TARGET=Linux
 METHOD=Linux
 PHASES=export_hdrs export_libs resources rom install_rom join
 
-RPCEMU_BIN=rpcemu/rpcemu
-BOOT_IOMD_ROM=boot_iomd_rom
-
-SHELL=/bin/bash
-.SHELLFLAGS=-e -c
-.ONESHELL:
-
-.DELETE_ON_ERROR:
-.PHONY: all build check fast
-
-# *Set Alias$$Obey %%Obey -v %%*0\n
+.PHONY: update-binary build check fast
 
 all: check
 
-ifeq (${METHOD}, rpcemu)
-build: rpcemu/rpcemu boot_iomd_rom stamp-prepare
+RISC_OS: $(if $(wildcard RISC_OS),,check)
+
+build_binds = $(foreach dir,bsd castle cddl gpl mixed,--ro-bind $(dir) /src/$(dir)) --bind build /build --ro-bind '${ACORN_CPP}' /AcornC.C++
+
+ifeq ($(METHOD), rpcemu)
+build: Built/rpcemu/rpcemu Built/boot_iomd_rom
 else
-build: run ${LINUX_ROM} stamp-prepare
+build: Built/qemu_sandbox ${LINUX_ROM}
 endif
+	set -o pipefail
 	uname -a
-	(
-	  echo Building GIT commit: $$(git rev-parse HEAD)
-ifeq (${TARGET}, Linux)
-	  echo '#define VERSION "GIT commit: '$$(git rev-parse HEAD)'\n"' > mixed/Linux/HAL/h/version1
-	  cmp --quiet mixed/Linux/HAL/h/version1 mixed/Linux/HAL/h/version || cp mixed/Linux/HAL/h/version1 mixed/Linux/HAL/h/version
+	export COMMIT="$$(git rev-parse HEAD)"
+	echo Building GIT commit: $$COMMIT
+	#
+	mkdir -p build
+	setup_build() {
+	  cd /build
+	  ! rm 'Images/$(TARGET)_rom'*
+	  mkdir -p Apps
+	  ln -sf /src/*/RiscOS/Apps/\!* Apps
+	  ln -sf mixed/RiscOS/{Library,Modules} castle/RiscOS/{Env,BuildSys} .
+	  cp -ru --preserve=mode,timestamps /src/castle/RiscOS/Export .
+	  ! cp -Hrsn /src/{bsd,castle,cddl,gpl,mixed} . 2>/dev/null
+ifeq ($(TARGET), Linux)
+	  echo '#define VERSION "GIT commit: '$$COMMIT'\n"' > version
+	  cmp --quiet version mixed/Linux/HAL/h/version || cp version mixed/Linux/HAL/h/version
 endif
-	  rm 'Images/${TARGET}_rom'* || true
-	)
-ifeq (${METHOD}, rpcemu)
-	ln -sfn /dev/fd/9 AcornC.C++
-	echo '*Obey -v mixed.Linux.Support.Build rpcemu HostFS:$$ HostFS:$$.AcornC/C++ ${TARGET} ${PHASES}' > '!Boot,fea'
-	rpcemu/rpcemu 7<boot_iomd_rom 4<. 9<'${ACORN_CPP}'
+	}
+	export -f setup_build
+	$(call sandbox_base, -s) $(sandbox_misc) $(build_binds) --dev-bind /dev/null /dev/null bash -x -e -c setup_build
+	#
+ifeq ($(METHOD), rpcemu)
+	echo '*Obey -v HostFS:$$.build.mixed.Linux.Support.Build rpcemu HostFS:$$.build HostFS:$$.AcornC/C++ $(TARGET) $(PHASES)' | \
+	$(BWRAP) --unshare-pid --unshare-net --ro-bind /tmp/.X11-unix /tmp/.X11-unix --proc /proc $(sandbox_misc) \
+	--ro-bind Built/rpcemu /r --ro-bind Built/boot_iomd_rom /r/roms/ROM --tmpfs /r/hostfs --dev-bind /dev/null /r/hd4.hdf $(build_binds) \
+	--symlink /build /r/hostfs/build --symlink /AcornC.C++ /r/hostfs/AcornC.C++ --file 0 '/r/hostfs/!Boot,fea' /r/rpcemu
 else
-	RISC_OS_Alias_IXFSBoot='Obey -v IXFS:$$.dev.fd.4.Build Linux IXFS:$$.dev.fd.8 IXFS:$$.dev.fd.9 ${TARGET} ${PHASES}' ./run ${LINUX_ROM} --nofork 4<mixed/Linux/Support 8<. 9<'${ACORN_CPP}' <<END
+	. Built/qemu_sandbox
+	env -i JOBS='$(JOBS)' RISC_OS_Alias_IXFSBoot='Obey -v IXFS:$$.src.mixed.Linux.Support.Build Linux IXFS:$$.build IXFS:$$.AcornC/C++ $(TARGET) $(PHASES)' $(sandbox_base) $(build_binds) --ro-bind '${LINUX_ROM}' /RISC_OS $$QEMU_sandbox $$QEMU /RISC_OS --nofork <<END 2>&1 | cat
 	*BASIC
 	VDU 7
 	SYS "IXSupport_LinuxSyscall",20,,,,,,,1
 	END
 endif
-	mv 'Images/${TARGET}_rom',??? 'Images/${TARGET}_rom' || true
-	chmod +x 'Images/${TARGET}_rom'
-	ln -f Images/Linux_rom RISC_OS || true
-	$(MAKE) sdl || true
-
-ifeq (${TARGET}, IOMD32)
-check: rpcemu/rpcemu HardDisc4/stamp
-	mixed/Linux/Tests/runner_rpcemu.sh Images/${TARGET}_rom'
+	find build/Images -type l -delete
+	! mv 'build/Images/$(TARGET)_rom',??? 'build/Images/$(TARGET)_rom'
+	! setfattr -n user.RISC_OS.LoadExec -v 0x00e5ffff00000000 'build/Images/$(TARGET)_rom'
+	true
+ifeq ($(TARGET), Linux)
+	chmod +x 'build/Images/$(TARGET)_rom'
+	ln -f build/Images/Linux_rom build/Images/Linux_rom_keep
+	mv build/Images/Linux_rom_keep RISC_OS
 endif
 
-ifeq (${TARGET}, Linux)
-check: run comma2attr
-	exec 9<.
-	mixed/Linux/Tests/runner.sh /dev/fd/9/run ./RISC_OS
+ifeq ($(TARGET), IOMD32)
+check: Built/rpcemu/rpcemu Built/rpcemu_sandbox
+	mixed/Linux/Tests/runner_rpcemu.sh Images/$(TARGET)_rom'
+endif
+
+ifeq ($(TARGET), Linux)
+check: Built/qemu_sandbox
+	if unshare -Umrnpfi true; then
+	  mixed/Linux/Tests/runner.sh ./RISC_OS
+	fi
 endif
 
 fast: PHASES=install_rom join
 fast: check
 check: build
 
-stamp-prepare: mixed/Linux/Support/build.mk
-	ln -sfn  mixed/RiscOS/Library
-	ln -sfn  mixed/RiscOS/Modules
-	ln -sfn castle/RiscOS/Env
-	ln -sfn castle/RiscOS/BuildSys
-	cp -r --preserve=mode,timestamps castle/RiscOS/Export .
-	mkdir -p Apps
-	(cd Apps && ln -sfn ../*/RiscOS/Apps/\!* .)
-	touch stamp-prepare
+Support/bin/!Boot/Linux/RISC_OS:
+	git submodule init
+	git submodule update
 
-run: ${QEMU}
-	ln -sfn ${QEMU} run
+update-binary:
+	+shopt -s extglob
+	unshare -Umrnpfi true
+ifndef FAST
+	git clean -fxd
+	git reset --hard
+endif
+	(cd mixed/Linux/Support/bin && git reset --hard && git clean -fxd && git checkout master~0)
+	git submodule update
+	SOURCE=$$(git rev-parse HEAD)
+	#
+ifndef FAST
+	$(MAKE) JOBS=$(JOBS) METHOD=$(METHOD)
+endif
+	(cd Support/bin && git fetch --force https://github.com/TimothyEBaldwin/RO_Linux.git master:master && git checkout master)
+	rm -rf Support/bin/Support 'Support/bin/!Boot'
+	mkdir -p Support/bin/Support 'Support/bin/!Boot/Linux'
+	cp -v --reflink=auto --preserve=mode,xattr Support/{Keyboard.h,*.c,*.cpp,*.diff,Start_RISC_OS.desktop,Finish,common.mk,download.sh,rpcemu.cfg,run_RISC_OS} Support/bin/Support/
+	cp -v --reflink=auto --preserve=mode,xattr mixed/Linux/SocketKVM/h/protocol Support/bin/Support/protocol.h
+	cp -v --reflink=auto --preserve=mode,xattr RISC_OS Support/bin/RISC_OS
+	ln -s '../../RISC_OS' 'Support/bin/!Boot/Linux/RISC_OS'
+	ln -sf 'Support/Start_RISC_OS.desktop' 'Support/bin/Start_RISC_OS.desktop'
+	#
+	echo "# Source and build GIT commits
+	SOURCE=$$SOURCE
+ifneq ($(METHOD), rpcemu)
+	BINARY=$$(cd Support/bin && git rev-parse HEAD)
+endif
+	LINUX='$$(uname -a)'
+	" > Support/bin/Support/source
+	#
+	(
+	cd Support/bin
+	git add -A
+	git commit -m "Rebuilt binary from $$SOURCE"
+	)
+	git add -u
+	git commit -m "Rebuilt bootstrap binary"
 
-include mixed/Linux/Support/common.mk
+
--- mixed/Linux/Tests/runner.sh
+++ mixed/Linux/Tests/runner.sh
@@ -2,30 +2,39 @@
 
 set -e
 
-if [ "$RISC_OS_Test_NetOK" == "YES" ]; then
-  # Configure network
-  ip link set lo up
-else
-  # Create namespace if possible
-  if unshare --user --net --mount --pid --fork --mount-proc --map-root-user true; then
-    export RISC_OS_Test_NetOK=YES
-    exec unshare --user --net --mount --pid --fork --mount-proc --map-root-user $0 $*
-  fi
-fi
-
-export RISC_OS_Test_Dir='IXFS:$.proc.self.cwd.mixed.Linux.Tests'
+export RISC_OS_Test_Dir='IXFS:$.Tests'
+export RISC_OS_Test_TmpDir='IXFS:$.tmp'
 export RISC_OS_Test_System='Linux'
 export RISC_OS_Test_Log='IXFS:$.proc.self.fd.2'
 export RISC_OS_Alias_TestLog='Echo %*0 { > <Test$Log> }'
+export RISC_OS_Test_NetOK=YES
+export risc_os="$1"
 
-# Tests break if redirected to a regular file, so ensure output though pipe
-exec > >(cat) 2>&1
+tests() {
+. Built/qemu_sandbox
+
+run() {
+  timeout --foreground -sKILL 60 \
+  ${BWRAP:=bwrap} --unshare-pid --proc /proc --dev /dev --dir /tmp --seccomp 9 9< <(Built/gen_seccomp) \
+  --ro-bind "$risc_os" /RISC_OS --ro-bind mixed/Linux/Tests /Tests $QEMU_sandbox $QEMU \
+  /RISC_OS "$@"
+}
+
+set -x
+
+# Configure network
+ip link set lo up
 
 # Test ptrace SWI implemnation
-RISC_OS_Alias_IXFSBoot='BASIC -quit <Test$Dir>.Finish' timeout --foreground -sKILL 30 $* --noseccomp --nofork --noaborts
+RISC_OS_Alias_IXFSBoot='BASIC -quit <Test$Dir>.Finish' run --noseccomp --nofork --noaborts
 
 # Various tests that shouldn't cause data aborts
-RISC_OS_Alias_IXFSBoot='Obey -v <Test$Dir>.PreDesk_NoAbort' timeout --foreground -sKILL 30 $* --nofork --noaborts
+RISC_OS_Alias_IXFSBoot='Obey -v <Test$Dir>.PreDesk_NoAbort' run --nofork --noaborts
 
 # Various tests that require data aborts
-RISC_OS_Alias_IXFSBoot='Obey -v <Test$Dir>.PreDesk_Aborts' timeout --foreground -sKILL 60 $* --nofork 3>/dev/null
+RISC_OS_Alias_IXFSBoot='Obey -v <Test$Dir>.PreDesk_Aborts' run --nofork
+}
+
+export -f tests
+# cat is needed here as stdout is reopened, which would reset the pointer of regular files.
+unshare -Umrn bash -e -c tests </dev/null 3>/dev/null 2>&1 | cat
