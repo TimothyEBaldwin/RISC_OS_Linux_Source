95d25f74249f67eae66a4db67c266c0504346598
Timothy E Baldwin <T.E.Baldwin99@members.leeds.ac.uk>
2018-06-02 13:47:24 +0100

    Linux Makefile: Remove certificate check on downloading
    Fixes downloads of Rpcemu
    Safe as downloaded files are checked against hashes.

--- mixed/Linux/Support/download.sh
+++ mixed/Linux/Support/download.sh
@@ -4,6 +4,6 @@ set -e
 leaf="$(basename "$1")"
 
 mkdir -p "$(dirname "$1")"
-wget -c -O "$leaf"  "$2"
+wget --no-check-certificate -c -O "$leaf"  "$2"
 echo "$3 *$leaf" | sha256sum -c
 mv "$leaf" "$1"
