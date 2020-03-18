0a541cd5d6251c395a68c524c3cdaf2aef5c99bc
Ben Avison <bavison@riscosopen.org>
2020-03-18 00:50:39 +0000

    Env: Backfill missing cross-compilation Env files

---
+++ RiscOS/Env/ROOL/S3C6410.sh
@@ -0,0 +1,27 @@
+# Environment to build ROM for Mini6410 board
+#
+# This script is applied to the current shell using the "." or "source" builtin shell commands.
+
+export LOCALE=UK
+export KEYBOARD=All
+export MACHINE=ARM11ZF
+export SYSTEM=Ursula
+export USERIF=Iyonix
+export DISPLAYTYPE=PAL
+export IMAGESIZE=4096K
+export HALSIZE=64K
+export BUILD=ROOL/S3C6410
+export APCS=APCS-32
+
+# Get the location of this script when sourced from a variety of common Posix shells: bash, dash, ksh or zsh
+
+if [ -n "$BASH" ]; then script=${BASH_SOURCE[0]}
+elif [ -n "$TMOUT" ]; then script=${.sh.file}
+elif [ -n "$ZSH_NAME" ]; then script=${(%):-%x}
+elif [ ${0##*/} = dash ]; then x=$(lsof -p $$ -Fn0 | tail -1); script=${x#*n}
+fi
+
+BUILDDIR="$(cd "$(dirname "$(readlink -f "$script")")"/../.. && pwd)"
+export BUILDDIR
+
+. "$BUILDDIR/Env/!Common.sh"
